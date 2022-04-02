4d712c0799e5a46564340c5e3586949a56cdc409
Sheumais <88064574+Sheumais@users.noreply.github.com>
2022-04-02 11:20:41 +1100

    Update hex.js

--- style/hex/hex.js
+++ style/hex/hex.js
@@ -1,1 +1,231 @@
-var _0x1d90=["\x63\x61\x6E\x76\x61\x73\x4F\x6E\x65","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x32\x64","\x67\x65\x74\x43\x6F\x6E\x74\x65\x78\x74","\x73\x71\x72\x74","\x6D\x6F\x75\x73\x65\x64\x6F\x77\x6E","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x70\x75\x73\x68","\x6C\x65\x6E\x67\x74\x68","\x67\x65\x74\x42\x6F\x75\x6E\x64\x69\x6E\x67\x43\x6C\x69\x65\x6E\x74\x52\x65\x63\x74","\x63\x6C\x69\x65\x6E\x74\x58","\x6C\x65\x66\x74","\x77\x69\x64\x74\x68","\x63\x6C\x69\x65\x6E\x74\x59","\x74\x6F\x70","\x68\x65\x69\x67\x68\x74","\x6D\x6F\x75\x73\x65\x6D\x6F\x76\x65","\x72\x65\x6D\x6F\x76\x65\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x6D\x6F\x75\x73\x65\x75\x70","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x72\x65\x74\x75\x72\x6E\x56\x61\x6C\x75\x65","\x61\x73\x69\x6E","\x50\x49","\x61\x62\x73","\x63\x6F\x73","\x73\x69\x6E","\x73\x6C\x69\x63\x65","\x61\x64\x64\x50\x6F\x69\x6E\x74","\x62\x65\x67\x69\x6E\x50\x61\x74\x68","\x6D\x6F\x76\x65\x54\x6F","\x6C\x69\x6E\x65\x54\x6F","\x67\x65\x74\x48\x75\x6C\x6C","\x78","\x79","\x73\x74\x72\x6F\x6B\x65\x53\x74\x79\x6C\x65","\x23\x30\x30\x30\x30\x36\x36","\x73\x74\x72\x6F\x6B\x65","\x66\x69\x6C\x6C\x53\x74\x79\x6C\x65","\x77\x68\x69\x74\x65","\x66\x69\x6C\x6C\x52\x65\x63\x74"];function canvasApp(){var _0xa67bx2=document[_0x1d90[1]](_0x1d90[0]);var _0xa67bx3=_0xa67bx2[_0x1d90[3]](_0x1d90[2]);_0xa67bx1b();var _0xa67bx4;var _0xa67bx5;var _0xa67bx6;var _0xa67bx7;var _0xa67bx8;var _0xa67bx9;var _0xa67bxa;var _0xa67bxb;var _0xa67bxc;var _0xa67bxd;var _0xa67bxe;var _0xa67bxf;var _0xa67bx10;var _0xa67bx11;var _0xa67bx12;var _0xa67bx13;var _0xa67bx14;var _0xa67bx15;var _0xa67bx16;var _0xa67bx17;var _0xa67bx18;var _0xa67bx19;var _0xa67bx1a;function _0xa67bx1b(){_0xa67bx4=[0,0];_0xa67bx5=[0,0];_0xa67bx6=0;_0xa67bxa=150;_0xa67bx7=0.1;_0xa67bx8=[_0xa67bx7,0];_0xa67bx9=[_0xa67bx7*0.5,_0xa67bx7*0.5*Math[_0x1d90[4]](3)];_0xa67bxb=[0,0];_0xa67bxc=[];_0xa67bxd=[];_0xa67bx1f();_0xa67bxe=[0.,0.];_0xa67bxf=0.;_0xa67bx3d();_0xa67bx2[_0x1d90[6]](_0x1d90[5],_0xa67bx23,false);}function _0xa67bx1c(_0xa67bx8,_0xa67bx9){var _0xa67bx1d=_0xa67bx8[0]-_0xa67bx9[0];var _0xa67bx1e=_0xa67bx8[1]-_0xa67bx9[1];return Math[_0x1d90[4]](_0xa67bx1d*_0xa67bx1d+_0xa67bx1e*_0xa67bx1e);}function _0xa67bx1f(){for(var _0xa67bx20=-30;_0xa67bx20<30;++_0xa67bx20){for(var _0xa67bx21=-30;_0xa67bx21<30;++_0xa67bx21){var _0xa67bx22=[_0xa67bx8[0]*_0xa67bx20+_0xa67bx9[0]*_0xa67bx21,_0xa67bx8[1]*_0xa67bx20+_0xa67bx9[1]*_0xa67bx21];if((_0xa67bx20-_0xa67bx21)%3!=0){_0xa67bxc[_0x1d90[7]](_0xa67bx22)};}};for(var _0xa67bx20=0;_0xa67bx20<_0xa67bxc[_0x1d90[8]];++_0xa67bx20){for(var _0xa67bx21=_0xa67bx20+1;_0xa67bx21<_0xa67bxc[_0x1d90[8]];++_0xa67bx21){if(_0xa67bx1c(_0xa67bxc[_0xa67bx20],_0xa67bxc[_0xa67bx21])<_0xa67bx7*1.01){_0xa67bxd[_0x1d90[7]]([_0xa67bx20,_0xa67bx21])}}};}function _0xa67bx23(_0xa67bx24){var _0xa67bx25=_0xa67bx2[_0x1d90[9]]();_0xa67bx11=2*(_0xa67bx24[_0x1d90[10]]-_0xa67bx25[_0x1d90[11]])/_0xa67bx25[_0x1d90[12]]-1;_0xa67bx12=2*(_0xa67bx24[_0x1d90[13]]-_0xa67bx25[_0x1d90[14]])/_0xa67bx25[_0x1d90[15]]-1;_0xa67bx13=_0xa67bx11;_0xa67bx14=_0xa67bx12;_0xa67bx10=true;_0xa67bx4=[_0xa67bx11,_0xa67bx12];if(_0xa67bx10){window[_0x1d90[6]](_0x1d90[16],_0xa67bx2b,false);_0xa67bx17=setInterval(_0xa67bx26,1000/60);};_0xa67bx2[_0x1d90[17]](_0x1d90[5],_0xa67bx23,false);window[_0x1d90[6]](_0x1d90[18],_0xa67bx27,false);if(_0xa67bx24[_0x1d90[19]]){_0xa67bx24[_0x1d90[19]]()}else {if(_0xa67bx24[_0x1d90[20]]){_0xa67bx24[_0x1d90[20]]=false}};return false;}function _0xa67bx26(){if(!_0xa67bx10){clearInterval(_0xa67bx17)};_0xa67bx3d();}function _0xa67bx27(_0xa67bx24){_0xa67bx2[_0x1d90[6]](_0x1d90[5],_0xa67bx23,false);window[_0x1d90[17]](_0x1d90[18],_0xa67bx27,false);if(_0xa67bx10){_0xa67bx10=false;window[_0x1d90[17]](_0x1d90[16],_0xa67bx2b,false);};}function _0xa67bx28(_0xa67bx8){return Math[_0x1d90[4]](_0xa67bx8[0]*_0xa67bx8[0]+_0xa67bx8[1]*_0xa67bx8[1])}function _0xa67bx29(_0xa67bx8,_0xa67bx9){var _0xa67bx2a=Math[_0x1d90[21]]((_0xa67bx8[0]*_0xa67bx9[1]-_0xa67bx8[1]*_0xa67bx9[0])/_0xa67bx28(_0xa67bx8)/_0xa67bx28(_0xa67bx9));if(_0xa67bx8[0]*_0xa67bx9[0]+_0xa67bx8[1]*_0xa67bx9[1]<0){if(_0xa67bx2a>0){_0xa67bx2a=Math[_0x1d90[22]]-_0xa67bx2a}else {_0xa67bx2a=-Math[_0x1d90[22]]-_0xa67bx2a}};return _0xa67bx2a;}function _0xa67bx2b(_0xa67bx24){var _0xa67bx25=_0xa67bx2[_0x1d90[9]]();_0xa67bx11=2*(_0xa67bx24[_0x1d90[10]]-_0xa67bx25[_0x1d90[11]])/_0xa67bx25[_0x1d90[12]]-1;_0xa67bx12=2*(_0xa67bx24[_0x1d90[13]]-_0xa67bx25[_0x1d90[14]])/_0xa67bx25[_0x1d90[15]]-1;_0xa67bx4=[_0xa67bx13,_0xa67bx14];_0xa67bx5=[_0xa67bx11,_0xa67bx12];_0xa67bxb[0]=_0xa67bx5[0]-_0xa67bx4[0];_0xa67bxb[1]=_0xa67bx5[1]-_0xa67bx4[1];var _0xa67bx2c;if(_0xa67bx28(_0xa67bxb)<1e-6){_0xa67bx2c=_0xa67bx28(_0xa67bx4)}else {_0xa67bx2c=_0xa67bx28(_0xa67bx4)-Math[_0x1d90[23]](_0xa67bxb[0]*_0xa67bx4[0]+_0xa67bxb[1]*_0xa67bx4[1])/_0xa67bx28(_0xa67bxb)};if(_0xa67bx2c<0.){_0xa67bx2c=0.};if(_0xa67bx2c>1.){_0xa67bx2c=1.};_0xa67bx6=_0xa67bx29(_0xa67bx5,_0xa67bx4)*_0xa67bx2c;_0xa67bxb[0]*=(1.-_0xa67bx2c);_0xa67bxb[1]*=(1.-_0xa67bx2c);for(var _0xa67bx20=0;_0xa67bx20<_0xa67bxc[_0x1d90[8]];++_0xa67bx20){_0xa67bxc[_0xa67bx20]=_0xa67bx30(_0xa67bxc[_0xa67bx20],-_0xa67bx6,_0xa67bxb)};_0xa67bxe=_0xa67bx30(_0xa67bxe,-_0xa67bx6,_0xa67bxb);_0xa67bxf-=_0xa67bx6;while(_0xa67bx28(_0xa67bxe)>3.*_0xa67bx7){var _0xa67bx2d=2e19;var _0xa67bx2e=-1;for(var _0xa67bx20=0;_0xa67bx20<6;++_0xa67bx20){var _0xa67bx2f=_0xa67bxe[0]*Math[_0x1d90[24]](_0xa67bxf+Math[_0x1d90[22]]/3.*_0xa67bx20)+_0xa67bxe[1]*Math[_0x1d90[25]](_0xa67bxf+Math[_0x1d90[22]]/3.*_0xa67bx20);if(_0xa67bx2f<_0xa67bx2d){_0xa67bx2d=_0xa67bx2f;_0xa67bx2e=_0xa67bx20;};};corr=[3.*_0xa67bx7*Math[_0x1d90[24]](_0xa67bxf+Math[_0x1d90[22]]/3.*_0xa67bx2e),3.*_0xa67bx7*Math[_0x1d90[25]](_0xa67bxf+Math[_0x1d90[22]]/3.*_0xa67bx2e)];_0xa67bxe=_0xa67bx30(_0xa67bxe,0.,corr);for(var _0xa67bx20=0;_0xa67bx20<_0xa67bxc[_0x1d90[8]];++_0xa67bx20){_0xa67bxc[_0xa67bx20]=_0xa67bx30(_0xa67bxc[_0xa67bx20],0.,corr)};};_0xa67bx13=_0xa67bx11;_0xa67bx14=_0xa67bx12;}function _0xa67bx30(_0xa67bx22,_0xa67bx8,_0xa67bx31){var _0xa67bx32=[Math[_0x1d90[24]](_0xa67bx8)*_0xa67bx22[0]-Math[_0x1d90[25]](_0xa67bx8)*_0xa67bx22[1],Math[_0x1d90[25]](_0xa67bx8)*_0xa67bx22[0]+Math[_0x1d90[24]](_0xa67bx8)*_0xa67bx22[1]];var _0xa67bx33=[_0xa67bx32[0]+_0xa67bx31[0],_0xa67bx32[1]+_0xa67bx31[1]];return _0xa67bx33;}function _0xa67bx34(){var _0xa67bx35= new ConvexHullGrahamScan();var _0xa67bx36=[];var _0xa67bx37=[];for(var _0xa67bx20=0;_0xa67bx20<_0xa67bxc[_0x1d90[8]];++_0xa67bx20){var _0xa67bx22=_0xa67bxc[_0xa67bx20][_0x1d90[26]]();var _0xa67bx38=_0xa67bx28(_0xa67bx22);_0xa67bx37[_0x1d90[7]](_0xa67bx38>Math[_0x1d90[22]]*0.5);if(_0xa67bx38!=0){_0xa67bx22[0]*=Math[_0x1d90[25]](_0xa67bx38)/_0xa67bx38;_0xa67bx22[1]*=Math[_0x1d90[25]](_0xa67bx38)/_0xa67bx38;};_0xa67bx36[_0x1d90[7]](_0xa67bx22);if(!_0xa67bx37[_0xa67bx20]){_0xa67bx35[_0x1d90[27]](_0xa67bx22[0],_0xa67bx22[1])};};_0xa67bx3[_0x1d90[28]]();for(var _0xa67bx20=0;_0xa67bx20<_0xa67bxd[_0x1d90[8]];++_0xa67bx20){if(_0xa67bx37[_0xa67bxd[_0xa67bx20][0]]||_0xa67bx37[_0xa67bxd[_0xa67bx20][1]]){continue };var _0xa67bx39=_0xa67bx36[_0xa67bxd[_0xa67bx20][0]];var _0xa67bx3a=_0xa67bx36[_0xa67bxd[_0xa67bx20][1]];_0xa67bx3[_0x1d90[29]](_0xa67bx39[0]*_0xa67bxa+_0xa67bxa,_0xa67bx39[1]*_0xa67bxa+_0xa67bxa);_0xa67bx3[_0x1d90[30]](_0xa67bx3a[0]*_0xa67bxa+_0xa67bxa,_0xa67bx3a[1]*_0xa67bxa+_0xa67bxa);};var _0xa67bx3b=_0xa67bx35[_0x1d90[31]]();var _0xa67bx3c=_0xa67bx3b[_0xa67bx3b[_0x1d90[8]]-1];_0xa67bx3[_0x1d90[29]](_0xa67bx3c[_0x1d90[32]]*_0xa67bxa+_0xa67bxa,_0xa67bx3c[_0x1d90[33]]*_0xa67bxa+_0xa67bxa);for(_0xa67bx20=0;_0xa67bx20<_0xa67bx3b[_0x1d90[8]];++_0xa67bx20){_0xa67bx3[_0x1d90[30]](_0xa67bx3b[_0xa67bx20][_0x1d90[32]]*_0xa67bxa+_0xa67bxa,_0xa67bx3b[_0xa67bx20][_0x1d90[33]]*_0xa67bxa+_0xa67bxa);_0xa67bx3[_0x1d90[29]](_0xa67bx3b[_0xa67bx20][_0x1d90[32]]*_0xa67bxa+_0xa67bxa,_0xa67bx3b[_0xa67bx20][_0x1d90[33]]*_0xa67bxa+_0xa67bxa);};_0xa67bx3[_0x1d90[34]]=_0x1d90[35];_0xa67bx3[_0x1d90[36]]();}function _0xa67bx3d(){_0xa67bx3[_0x1d90[37]]=_0x1d90[38];_0xa67bx3[_0x1d90[39]](0,0,_0xa67bx2[_0x1d90[12]],_0xa67bx2[_0x1d90[15]]);_0xa67bx34();}}
+function canvasApp() {
+  var lurine = document.getElementById("canvasOne");
+  var ivyn = lurine.getContext("2d");
+  quevon();
+  var shashi;
+  var taleka;
+  var elizabith;
+  var shatyra;
+  var jong;
+  var caiyden;
+  var mihcael;
+  var alo;
+  var judalon;
+  var zhyla;
+  var shavada;
+  var zamyia;
+  var oluwabukola;
+  var anaysa;
+  var makayley;
+  var jaymi;
+  var tiawan;
+  var larce;
+  var chayse;
+  var mabil;
+  var ellany;
+  var larkynn;
+  var nareli;
+  function quevon() {
+    shashi = [0, 0];
+    taleka = [0, 0];
+    elizabith = 0;
+    mihcael = 150;
+    shatyra = .1;
+    jong = [shatyra, 0];
+    caiyden = [shatyra * .5, shatyra * .5 * Math.sqrt(3)];
+    alo = [0, 0];
+    judalon = [];
+    zhyla = [];
+    armend();
+    shavada = [0, 0];
+    zamyia = 0;
+    sidora();
+    lurine.addEventListener("mousedown", stearl, false);
+  }
+  function hirvin(quashonda, latona) {
+    var danalynn = quashonda[0] - latona[0];
+    var jessilyn = quashonda[1] - latona[1];
+    return Math.sqrt(danalynn * danalynn + jessilyn * jessilyn);
+  }
+  function armend() {
+    for (var emmanuelle = -30; emmanuelle < 30; ++emmanuelle) {
+      for (var truvy = -30; truvy < 30; ++truvy) {
+        var quayshun = [jong[0] * emmanuelle + caiyden[0] * truvy, jong[1] * emmanuelle + caiyden[1] * truvy];
+        if ((emmanuelle - truvy) % 3 != 0) {
+          judalon.push(quayshun);
+        }
+        ;
+      }
+    }
+    ;
+    for (var emmanuelle = 0; emmanuelle < judalon.length; ++emmanuelle) {
+      for (var truvy = emmanuelle + 1; truvy < judalon.length; ++truvy) {
+        if (hirvin(judalon[emmanuelle], judalon[truvy]) < shatyra * 1.01) {
+          zhyla.push([emmanuelle, truvy]);
+        }
+      }
+    }
+    ;
+  }
+  function stearl(aryash) {
+    var kurtina = lurine.getBoundingClientRect();
+    anaysa = 2 * (aryash.clientX - kurtina.left) / kurtina.width - 1;
+    makayley = 2 * (aryash.clientY - kurtina.top) / kurtina.height - 1;
+    jaymi = anaysa;
+    tiawan = makayley;
+    oluwabukola = true;
+    shashi = [anaysa, makayley];
+    if (oluwabukola) {
+      window.addEventListener("mousemove", derrell, false);
+      mabil = setInterval(bwana, 16.666666666666668);
+    }
+    ;
+    lurine.removeEventListener("mousedown", stearl, false);
+    window.addEventListener("mouseup", mykalla, false);
+    if (aryash.preventDefault) {
+      aryash.preventDefault();
+    } else {
+      if (aryash.returnValue) {
+        aryash.returnValue = false;
+      }
+    }
+    ;
+    return false;
+  }
+  function bwana() {
+    if (!oluwabukola) {
+      clearInterval(mabil);
+    }
+    ;
+    sidora();
+  }
+  function mykalla(raidyn) {
+    lurine.addEventListener("mousedown", stearl, false);
+    window.removeEventListener("mouseup", mykalla, false);
+    if (oluwabukola) {
+      oluwabukola = false;
+      window.removeEventListener("mousemove", derrell, false);
+    }
+    ;
+  }
+  function maydee(lucritia, esiquio) {
+    var jenetta = Math.asin((lucritia[0] * esiquio[1] - lucritia[1] * esiquio[0]) / Math.sqrt(lucritia[0] * lucritia[0] + lucritia[1] * lucritia[1]) / Math.sqrt(esiquio[0] * esiquio[0] + esiquio[1] * esiquio[1]));
+    if (lucritia[0] * esiquio[0] + lucritia[1] * esiquio[1] < 0) {
+      if (jenetta > 0) {
+        jenetta = Math.PI - jenetta;
+      } else {
+        jenetta = -Math.PI - jenetta;
+      }
+    }
+    ;
+    return jenetta;
+  }
+  function derrell(marietha) {
+    var iline = lurine.getBoundingClientRect();
+    anaysa = 2 * (marietha.clientX - iline.left) / iline.width - 1;
+    makayley = 2 * (marietha.clientY - iline.top) / iline.height - 1;
+    shashi = [jaymi, tiawan];
+    taleka = [anaysa, makayley];
+    alo[0] = taleka[0] - shashi[0];
+    alo[1] = taleka[1] - shashi[1];
+    var tamlyn;
+    if (Math.sqrt(alo[0] * alo[0] + alo[1] * alo[1]) < .000001) {
+      tamlyn = Math.sqrt(shashi[0] * shashi[0] + shashi[1] * shashi[1]);
+    } else {
+      tamlyn = Math.sqrt(shashi[0] * shashi[0] + shashi[1] * shashi[1]) - Math.abs(alo[0] * shashi[0] + alo[1] * shashi[1]) / Math.sqrt(alo[0] * alo[0] + alo[1] * alo[1]);
+    }
+    ;
+    if (tamlyn < 0) {
+      tamlyn = 0;
+    }
+    ;
+    if (tamlyn > 1) {
+      tamlyn = 1;
+    }
+    ;
+    elizabith = maydee(taleka, shashi) * tamlyn;
+    alo[0] *= 1 - tamlyn;
+    alo[1] *= 1 - tamlyn;
+    for (var jelisha = 0; jelisha < judalon.length; ++jelisha) {
+      judalon[jelisha] = avlyn(judalon[jelisha], -elizabith, alo);
+    }
+    ;
+    shavada = avlyn(shavada, -elizabith, alo);
+    zamyia -= elizabith;
+    while (Math.sqrt(shavada[0] * shavada[0] + shavada[1] * shavada[1]) > 3 * shatyra) {
+      var jasur = 2e19;
+      var rosanny = -1;
+      for (var jelisha = 0; jelisha < 6; ++jelisha) {
+        var zeron = shavada[0] * Math.cos(zamyia + Math.PI / 3 * jelisha) + shavada[1] * Math.sin(zamyia + Math.PI / 3 * jelisha);
+        if (zeron < jasur) {
+          jasur = zeron;
+          rosanny = jelisha;
+        }
+        ;
+      }
+      ;
+      corr = [3 * shatyra * Math.cos(zamyia + Math.PI / 3 * rosanny), 3 * shatyra * Math.sin(zamyia + Math.PI / 3 * rosanny)];
+      shavada = avlyn(shavada, 0, corr);
+      for (var jelisha = 0; jelisha < judalon.length; ++jelisha) {
+        judalon[jelisha] = avlyn(judalon[jelisha], 0, corr);
+      }
+      ;
+    }
+    ;
+    jaymi = anaysa;
+    tiawan = makayley;
+  }
+  function avlyn(jyena, jimika, amalea) {
+    var yanely = [Math.cos(jimika) * jyena[0] - Math.sin(jimika) * jyena[1], Math.sin(jimika) * jyena[0] + Math.cos(jimika) * jyena[1]];
+    var niasia = [yanely[0] + amalea[0], yanely[1] + amalea[1]];
+    return niasia;
+  }
+  function izabella() {
+    var hanner = new ConvexHullGrahamScan;
+    var thurston = [];
+    var luisana = [];
+    for (var robinn = 0; robinn < judalon.length; ++robinn) {
+      var sirvon = judalon[robinn].slice();
+      var gordana = Math.sqrt(sirvon[0] * sirvon[0] + sirvon[1] * sirvon[1]);
+      luisana.push(gordana > Math.PI * .5);
+      if (gordana != 0) {
+        sirvon[0] *= Math.sin(gordana) / gordana;
+        sirvon[1] *= Math.sin(gordana) / gordana;
+      }
+      ;
+      thurston.push(sirvon);
+      if (!luisana[robinn]) {
+        hanner.addPoint(sirvon[0], sirvon[1]);
+      }
+      ;
+    }
+    ;
+    ivyn.beginPath();
+    for (var robinn = 0; robinn < zhyla.length; ++robinn) {
+      if (luisana[zhyla[robinn][0]] || luisana[zhyla[robinn][1]]) {
+        continue;
+      }
+      ;
+      var tazmin = thurston[zhyla[robinn][0]];
+      var derrien = thurston[zhyla[robinn][1]];
+      ivyn.moveTo(tazmin[0] * mihcael + mihcael, tazmin[1] * mihcael + mihcael);
+      ivyn.lineTo(derrien[0] * mihcael + mihcael, derrien[1] * mihcael + mihcael);
+    }
+    ;
+    var ericberto = hanner.getHull();
+    var oreta = ericberto[ericberto.length - 1];
+    ivyn.moveTo(oreta.x * mihcael + mihcael, oreta.y * mihcael + mihcael);
+    for (robinn = 0; robinn < ericberto.length; ++robinn) {
+      ivyn.lineTo(ericberto[robinn].x * mihcael + mihcael, ericberto[robinn].y * mihcael + mihcael);
+      ivyn.moveTo(ericberto[robinn].x * mihcael + mihcael, ericberto[robinn].y * mihcael + mihcael);
+    }
+    ;
+    ivyn.strokeStyle = "#000066";
+    ivyn.stroke();
+  }
+  function sidora() {
+    ivyn.fillStyle = "white";
+    ivyn.fillRect(0, 0, lurine.width, lurine.height);
+    izabella();
+  }
+}
