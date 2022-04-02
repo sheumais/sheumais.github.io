1b89b148c9a888654880621331dab33b51bea8de
Sheumais <88064574+Sheumais@users.noreply.github.com>
2022-04-03 00:35:09 +1100

    Update hex.js

--- style/hex/hex.js
+++ style/hex/hex.js
@@ -379,7 +379,7 @@ function canvasApp() {
       ivyn.moveTo(ericberto[robinn].x * mihcael + mihcael, ericberto[robinn].y * mihcael + mihcael);
     }
     ;
-    ivyn.strokeStyle = "#000000";
+    ivyn.strokeStyle = "#ffc800";
     ivyn.stroke();
   }
   function sidora() {
